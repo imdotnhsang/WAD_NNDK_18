a0ed1bee1290dae3f2be60b19d9f27264258db85
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-04-22 22:28:39 +0700

    update done menu

--- Source/js/script.js
+++ Source/js/script.js
@@ -1,18 +1,28 @@
 var checkHamburger = false;
-$('.hamburger').click(function() {
+$('.hamburger').click(function () {
     if (checkHamburger == false) {
         $(this).addClass("toggle");
-        $('.nav-link-custom').click(function() {
+        $('.nav-link-custom').click(function () {
             $(this).parent().children('ul').slideToggle();
         });
-        // $('body,html').css("overflow-y", "hidden");
-        // $('body,html').css("-webkit-overflow-y", "hidden");
+        var checkOpenNLink = false;
+        $('.child-list-nav-links').click(function () {
+            var index = $(this).val();
+            var checkO = $(this).attr("checkOpen");
+            if (checkO == 'false') {
+                $('.navbar-nav-custom li:nth-child(' + index + ') a i').css('color', '#fff');
+                $('.navbar-nav-custom li:nth-child(' + index + ') a i').css('transform', 'rotate(-180deg)');
+                $(this).attr("checkOpen","true");
+            } else {
+                $('.navbar-nav-custom li:nth-child(' + index + ') a i').css('color', '#dd3333');
+                $('.navbar-nav-custom li:nth-child(' + index + ') a i').css('transform', 'rotate(0deg)');
+                $(this).attr("checkOpen","false");
+            }
+        });
         checkHamburger = true;
     } else {
         $(this).removeClass("toggle");
         $('.nav-link-custom').off('click');
-        // $('body,html').css("overflow-y", "visible");
-        // $('body,html').css("-webkit-overflow-y", "visible");
         checkHamburger = false;
     }
 });
