faabfcccec4a9f8e7bc296a613244f7428ce2947
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-04-22 16:59:21 +0700

    update 99.9% nav

--- Source/js/script.js
+++ Source/js/script.js
@@ -5,12 +5,14 @@ $('.hamburger').click(function() {
         $('.nav-link-custom').click(function() {
             $(this).parent().children('ul').slideToggle();
         });
-        $('body,html').css("overflow-y", "hidden");
+        // $('body,html').css("overflow-y", "hidden");
+        // $('body,html').css("-webkit-overflow-y", "hidden");
         checkHamburger = true;
     } else {
         $(this).removeClass("toggle");
         $('.nav-link-custom').off('click');
-        $('body,html').css("overflow-y", "visible");
+        // $('body,html').css("overflow-y", "visible");
+        // $('body,html').css("-webkit-overflow-y", "visible");
         checkHamburger = false;
     }
 });
