9bfa35a72099ecb749e5fb536b51bb949bdbd254
VinhPhat207 <dvp207@gmail.com>
2019-04-24 14:41:41 +0700

    add 3 table (users.html) + administrator script

---
+++ Source/js/administrator/administrator.js
@@ -0,0 +1,19 @@
+$('#js-user-type-select').change(function () {
+    switch ($(this).val()) {
+        case "subscriber":
+            $('#js-writer-table').removeAttr("style").hide();
+            $('#js-editor-table').removeAttr("style").hide();
+            $('#js-subscriber-table').show();
+            break;
+        case "writer":
+            $('#js-subscriber-table').removeAttr("style").hide();
+            $('#js-editor-table').removeAttr("style").hide();
+            $('#js-writer-table').show();
+            break;
+        case "editor":
+            $('#js-subscriber-table').removeAttr("style").hide();
+            $('#js-writer-table').removeAttr("style").hide();
+            $('#js-editor-table').show();
+            break;
+    }
+})
