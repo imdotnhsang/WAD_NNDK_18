2533e94ec95b91735a5d87b500be2b34ca95b9bb
dvphat207 <dvphat207@github.com>
2019-04-26 02:09:08 +0700

    clean js folder + handle edit and delete script

--- Source/js/administrator/administrator.js
+++ Source/js/administrator/administrator.js
@@ -1,5 +1,27 @@
-$('#js-user-type-select').change(function () {
-    switch ($(this).val()) {
+$('.js-edit-input').click(function() {
+    let inputTag = $(this).parent().parent().children('input');
+
+    if (inputTag.attr('disabled')) {
+        inputTag.removeAttr('disabled')
+        $(this).children().removeClass('far fa-edit');
+        $(this).children().addClass('fas fa-check');
+    } else {
+        inputTag.prop('disabled', 'true')
+        $(this).children().removeClass('fas fa-check');
+        $(this).children().addClass('far fa-edit');
+    }
+});
+
+$('.js-delete-input').click(function() {
+    let liTag = $(this).parent().parent().parent();
+    liTag.remove();
+});
+
+
+$('#js-user-type-select').change(function() {
+    let valSelected = $(this).val();
+    
+    switch (valSelected) {
         case "subscriber":
             $('#js-writer-table').removeAttr("style").hide();
             $('#js-editor-table').removeAttr("style").hide();
@@ -16,9 +38,7 @@ $('#js-user-type-select').change(function () {
             $('#js-editor-table').show();
             break;
     }
-})
+});
 
-function editHandle() {
-    console.log(this);
-    
-}
+
+
