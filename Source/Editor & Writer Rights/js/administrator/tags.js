f67ff48caf36a125e3ec3b1243ad4a54c4557d34
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-05-05 09:16:33 +0700

    update front-end

---
+++ Source/Editor & Writer Rights/js/administrator/tags.js
@@ -0,0 +1,53 @@
+const TAGS_PER_PAGE = 5;
+const tagsMockData = ['C++', 'PHP', 'Java', 'Javascript', 'ReactJS', 'NodeJS', 'Angular', 'HTML', 'CSS', 'Jquery', 'Bootstrap'];
+let tagPageNumber = 1;
+
+const updateTagsTable = pageNumber => {
+    const tagsTbody = $('.js-tags-table').children('tbody');
+
+    const start = (pageNumber - 1) * TAGS_PER_PAGE;
+    const end = Math.min(pageNumber * TAGS_PER_PAGE, tagsMockData.length);
+
+    tagsTbody.html('');
+    for (let i = start; i < end; i++) {
+        tagsTbody.html(
+            tagsTbody.html() +
+            `
+                <tr class="text-center">
+                    <td>${i + 1}</td>
+                    <td>${tagsMockData[i]}</td>
+                    <td>
+                        <button class="btn btn-sm btn-warning mr-1">
+                            <i class="material-icons">edit</i> Edit
+                        </button>
+                        <button class="btn btn-sm btn-danger">
+                            <i class="material-icons">delete</i> Delete
+                        </button>
+                    </td>
+                </tr>
+            `
+        );
+    };
+}
+
+$(document).ready(function () {
+    updateTagsTable(tagPageNumber);
+});
+
+$(document).on('click', 'li.page-item', function () {
+    const value = $(this).children('a').html().trim();
+    
+    switch (value) {
+        case '\253':
+            tagPageNumber = Math.max(1, tagPageNumber - 1);
+            break;
+        case '\273':
+            tagPageNumber = Math.min(Math.floor(tagsMockData.length / TAGS_PER_PAGE) + 1, tagPageNumber + 1);
+            break;
+        default:
+            break;
+    }
+    
+    document.getElementById('js-tags-page-number').innerHTML = tagPageNumber;
+    updateTagsTable(tagPageNumber);
+});
