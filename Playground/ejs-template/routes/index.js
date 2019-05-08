4703ccc7bda4d3953a2cbeff9afcaba280ddf5e0
Vinh Phat <dvphat207@github.com>
2019-05-09 00:07:54 +0700

    add public files to ejs-template

--- Playground/ejs-template/routes/index.js
+++ Playground/ejs-template/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/administrator', function(req, res, next) {
+  res.render('pages/administrator/index', { title: 'Express' });
+});
+
 module.exports = router;
