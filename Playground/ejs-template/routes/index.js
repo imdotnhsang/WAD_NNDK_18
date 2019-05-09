a4fd6d5b030434f4c4437c50b845b09ed54cceb7
Vinh Phat <dvphat207@github.com>
2019-05-09 21:39:12 +0700

    add partials + layout ejs (admistrator)

--- Playground/ejs-template/routes/index.js
+++ Playground/ejs-template/routes/index.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/administrator', function(req, res, next) {
-  res.render('pages/administrator/index', { title: 'Express' });
+router.get('/administrator/profile', function (req, res, next) {
+  res.render(
+    'pages/administrator',
+    {
+      title: 'Profile',
+      layout: 'layouts/profile',
+      srcScript: '/javascripts/profile.js'
+    }
+  );
 });
 
 module.exports = router;
