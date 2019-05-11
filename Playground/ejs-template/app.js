c9129efcb58ca1d1aa55447f5ee27eceae9c904e
Vinh Phat <dvphat207@github.com>
2019-05-11 15:33:19 +0700

    setup mongodb

--- Playground/ejs-template/app.js
+++ Playground/ejs-template/app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const { MONGO_URI } = process.env;
+
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true })
+    .then(_ => console.log("Connected to MongoDB."))
+    .catch(err =>
+        console.log("Connect to MongoDB with Error Message: ", err.errmsg)
+    );
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
