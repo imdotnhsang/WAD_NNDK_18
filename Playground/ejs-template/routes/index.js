9c098fc51c686d1d7b667432d219fda57f15b3c1
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-05-14 22:42:09 +0700

    update ejs guest, subscriber, log

--- Playground/ejs-template/routes/index.js
+++ Playground/ejs-template/routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// ADMINISTRATOR
+
 router.get('/administrator/profile', function (req, res, next) {
   res.render(
     'pages/administrator',
@@ -61,13 +63,172 @@ router.get('/administrator/users', function (req, res, next) {
   );
 });
 
+// subscriber
 router.get('/subscriber/home', function (req, res, next) {
   res.render(
     'pages/subscriber',
     {
       title: 'Home',
       layout: 'layouts/home',
-      srcScript: '/javascripts/guest-subscriber/script.js'
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: ''
+    }
+  );
+});
+
+router.get('/subscriber/detail', function (req, res, next) {
+  res.render(
+    'pages/subscriber',
+    {
+      title: 'Detail',
+      layout: 'layouts/detail',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/detail.css'
+    }
+  );
+});
+
+router.get('/subscriber/category', function (req, res, next) {
+  res.render(
+    'pages/subscriber',
+    {
+      title: 'Category',
+      layout: 'layouts/category',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/category.css'
+    }
+  );
+});
+
+router.get('/subscriber/hashtag', function (req, res, next) {
+  res.render(
+    'pages/subscriber',
+    {
+      title: 'Hashtag',
+      layout: 'layouts/hashtag',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/hashtag.css'
+    }
+  );
+});
+
+router.get('/subscriber/information', function (req, res, next) {
+  res.render(
+    'pages/subscriber',
+    {
+      title: 'Information',
+      layout: 'layouts/information',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/information.css'
+    }
+  );
+});
+
+router.get('/subscriber/search', function (req, res, next) {
+  res.render(
+    'pages/subscriber',
+    {
+      title: 'Search',
+      layout: 'layouts/search',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/search.css'
+    }
+  );
+});
+
+//guest 
+router.get('/guest/home', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Home',
+      layout: 'layouts/home',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: ''
+    }
+  );
+});
+
+router.get('/guest/detail', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Detail',
+      layout: 'layouts/detail',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/detail.css'
+    }
+  );
+});
+
+router.get('/guest/category', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Category',
+      layout: 'layouts/category',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/category.css'
+    }
+  );
+});
+
+router.get('/subscriber/hashtag', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Hashtag',
+      layout: 'layouts/hashtag',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/hashtag.css'
+    }
+  );
+});
+
+router.get('/guest/information', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Information',
+      layout: 'layouts/information',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/information.css'
+    }
+  );
+});
+
+router.get('/guest/search', function (req, res, next) {
+  res.render(
+    'pages/guest',
+    {
+      title: 'Search',
+      layout: 'layouts/search',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/search.css'
+    }
+  );
+});
+
+router.get('/sign/signin', function (req, res, next) {
+  res.render(
+    'pages/sign',
+    {
+      title: 'Sign In',
+      layout: 'layouts/signin',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/login.css'
+    }
+  );
+});
+
+router.get('/sign/signup', function (req, res, next) {
+  res.render(
+    'pages/sign',
+    {
+      title: 'Sign Up',
+      layout: 'layouts/signup',
+      srcScript: '/javascripts/guest-subscriber/script.js',
+      hrefCss: '/stylesheets/guest-subscriber/login.css'
     }
   );
 });
