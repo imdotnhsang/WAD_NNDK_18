33bca06e8bca531acb8fc367ffeeea2ea25e0d8e
hophongit98 <hophongit98@gmail.com>
2019-06-02 00:09:31 +0700

    writer add new post

--- Playground/ejs-template/public/javascripts/writer/addnewpost.js
+++ Playground/ejs-template/public/javascripts/writer/addnewpost.js
@@ -1,9 +1,15 @@
 CKEDITOR.replace('textarea_content');
 CKEDITOR.config.height = "500";
 
-$('#btn_publish').click(function(e){
+$('#btn_publish').click(function (e) {
     e.preventDefault();
-    
-   var data = CKEDITOR.instances['textarea_content'].getData();
-   alert(data);
+
+    var data = CKEDITOR.instances['textarea_content'].getData();
+    alert(data);
+});
+
+
+CKEDITOR.replace('textarea_content', {
+    "extraPlugins": 'imagebrowser',
+    "imageBrowser_listUrl": "/files"
 });
