5841c7033157dbac6a9c5a57e9d3fcc2f1def2be
hophongit98 <hophongit98@gmail.com>
2019-06-02 12:36:11 +0700

    writer add new post

--- Playground/ejs-template/routes/pages/writer.js
+++ Playground/ejs-template/routes/pages/writer.js
@@ -17,46 +17,42 @@ var storage = multer.diskStorage({
 })
 var upload = multer({ storage: storage });
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-
 //show ckeditor to home
-app.get('/', function (req, res) {
-  var title = "Plugin Imagebrowser ckeditor for nodejs"
-  res.render('index', { result: 'result' })
+router.get('/', function (req, res) {
+    var title = "Plugin Imagebrowser ckeditor for nodejs"
+    res.render('index', { result: 'result' })
 })
 
-app.get('/files', function (req, res) {
+router.get('/files', function (req, res) {
     const images = fs.readdirSync('public/upload')
     var sorted = []
-    for (let item of images){
-        if(item.split('.').pop() === 'png'
-        || item.split('.').pop() === 'jpg'
-        || item.split('.').pop() === 'jpeg'
-        || item.split('.').pop() === 'svg'){
+    for (let item of images) {
+        if (item.split('.').pop() === 'png'
+            || item.split('.').pop() === 'jpg'
+            || item.split('.').pop() === 'jpeg'
+            || item.split('.').pop() === 'svg') {
             var abc = {
-                  "image" : "/upload/"+item,
-                  "folder" : '/'
+                "image": "/upload/" + item,
+                "folder": '/'
             }
             sorted.push(abc)
         }
     }
     res.send(sorted);
-  })
+})
 
-  app.post('/upload', upload.array('flFileUpload', 12), function (req, res, next) {
-      res.redirect('back')
-  });
+router.post('/upload', upload.array('flFileUpload', 12), function (req, res, next) {
+    res.redirect('back')
+});
 
-  app.post('/delete_file', function(req, res, next){
-  	var url_del = 'public' + req.body.url_del
+router.post('/delete_file', function (req, res, next) {
+    var url_del = 'public' + req.body.url_del
     console.log(url_del)
-  	if(fs.existsSync(url_del)){
-  		fs.unlinkSync(url_del)
-  	}
-  	res.redirect('back')
-  });
+    if (fs.existsSync(url_del)) {
+        fs.unlinkSync(url_del)
+    }
+    res.redirect('back')
+});
 
 router.get('/add-new-post', function (req, res, next) {
     res.render(
