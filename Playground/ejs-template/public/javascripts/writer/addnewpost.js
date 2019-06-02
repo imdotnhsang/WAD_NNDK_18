c22a68ddc288beb96951428fba85cc4817513b46
hophongit98 <hophongit98@gmail.com>
2019-06-02 15:56:52 +0700

    update writer

--- Playground/ejs-template/public/javascripts/writer/addnewpost.js
+++ Playground/ejs-template/public/javascripts/writer/addnewpost.js
@@ -2,9 +2,9 @@
 $('#btn_publish').click(function (e) {
     e.preventDefault();
 
-    var data = CKEDITOR.instances['textarea_content'].getData();
-    var imgList = document.querySelectorAll('.easyimage');
-    alert(imgList.length);
+    var data = CKEDITOR.instances['editor'].getData();
+    
+    alert(data);
 });
 
 var quill = new Quill('#editor', {
@@ -16,6 +16,8 @@ CKEDITOR.replace('editor', {
     "extraPlugins": 'imagebrowser',
     "imageBrowser_listUrl": "/writer/files",
 });
+
+var category = [ 'tech', 'aa', 'bb'];
 
 // CKEDITOR.config.filebrowserImageUploadUrl = '/api/writer/upload';
 CKEDITOR.config.height = "500";
