777077105a7c65280c1570e62397f50f5f456983
dvphat207 <dvphat207@github.com>
2019-06-10 13:56:57 +0700

    fix bug pickUser

--- Full-stack/utils/pickUser.js
+++ Full-stack/utils/pickUser.js
@@ -1,13 +1,13 @@
 const _ = require('lodash');
 
 module.exports = (user, userType) => {
-    const result = _.pick(user, ['_id', 'username', 'email', 'avatar', 'birthday', 'fullname', 'confirmed', 'userType', 'gender']);
+    const result = _.pick(user, ['_id', 'username', 'email', 'avatar', 'birthday', 'fullname', 'confirmed', 'userType', 'gender', 'expiredAt']);
     
     console.log(result);
     switch (userType) {
-        case 'subscriber':
-            result.expiredAt = user.expiredAt;
-            break;
+        // case 'subscriber':
+        //     result.expiredAt = user.expiredAt;
+        //     break;
         case 'writer':
             result.pseudonym = user.pseudonym;
             break;
