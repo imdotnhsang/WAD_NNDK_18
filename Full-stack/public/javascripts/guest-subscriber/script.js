48295ba02941cce30df85188facbd70df60134c2
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-06-13 21:11:40 +0700

    update print pdf

--- Full-stack/public/javascripts/guest-subscriber/script.js
+++ Full-stack/public/javascripts/guest-subscriber/script.js
@@ -392,27 +392,42 @@ $('.container-detail .content-detail .main-content-detail img').parent('p').css(
 
 //transfer news to pdf
 $('#detail__print').click(function () {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    source = $('#newsDet__pdf')[0];
-    specialElementHandlers = {
-        '#bypassme': function (element, renderer) {
-            return true
-        }
-    }
-    margins = {
-        top: 50,
-        left: 60,
-        width: 545
-    };
-    pdf.fromHTML(
-        source
-        , margins.left
-        , margins.top
-        , {
-            'width': margins.width
-        },
-        function (dispose) {
-            pdf.save('newsTBW.pdf');
-        }
-    )
+    // var pdf = new jsPDF('p', 'pt', 'letter');
+    // source = $('#main-content-detail')[0];
+    // specialElementHandlers = {
+    //     '#bypassme': function (element, renderer) {
+    //         return true
+    //     }
+    // }
+    // css={
+        
+    // }
+    // pdf.fromHTML(
+    //     source,
+    //     function (dispose) {
+    //         pdf.save('newsTBW.pdf');
+    //     }
+    // )
+    $("#main-content-detail").printThis({
+        debug: false,               // show the iframe for debugging
+        importCSS: true,            // import parent page css
+        importStyle: false,         // import style tags
+        printContainer: true,       // print outer container/$.selector
+        loadCSS: "http://localhost:5000/stylesheets/guest-subscriber/detail.css",                // path to additional css file - use an array [] for multiple
+        pageTitle: "",              // add title to print page
+        removeInline: false,        // remove inline styles from print elements
+        removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
+        printDelay: 333,            // variable print delay
+        header: null,               // prefix to html
+        footer: null,               // postfix to html
+        base: false,                // preserve the BASE tag or accept a string for the URL
+        formValues: true,           // preserve input/form values
+        canvas: false,              // copy canvas content
+        doctypeString: '...',       // enter a different doctype for older markup
+        removeScripts: false,       // remove script tags from print content
+        copyTagClasses: false,      // copy classes from the html & body tag
+        beforePrintEvent: null,     // function for printEvent in iframe
+        beforePrint: null,          // function called before iframe is filled
+        afterPrint: null            // function called before iframe is removed
+    });
 });
