ba7df510f53aebbd27ae042b07dca6c41430924f
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-06-14 10:01:15 +0700

    update pp gg

--- Full-stack/configs/passport_google.js
+++ Full-stack/configs/passport_google.js
@@ -11,40 +11,4 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
-// passport.use('local.login', new LocalStrategy({
-//     usernameField: 'usernameOrEmail',
-//     passwordField: 'password',
-//     passReqToCallback: true
-// }, (req, usernameOrEmail, password, done) => {
-//     const errors = {};
 
-//     User
-//         .findOne({
-//             $and: [
-//                 { $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }] },
-//                 { isActive: true }
-//             ]
-//         }, (err, user) => {
-//             if (err) {
-//                 return done(err);
-//             }
-
-//             if (!user) {
-//                 errors.usernameOrEmail = 'Username or email not found.';
-//                 return done(errors);
-//             }
-//             if (!user.validPassword(password)) {
-//                 errors.password = "Password incorrect.";
-//                 return done(errors);
-//             }
-
-//             if (user.confirmed === false) {
-//                 errors.confirmed = 'Account must be confirmed email.'
-//                 return done(errors);
-//             }
-
-//             const payload = pickUser(user, user.userType);
-            
-//             return done(null, payload);
-//         });
-// }));
