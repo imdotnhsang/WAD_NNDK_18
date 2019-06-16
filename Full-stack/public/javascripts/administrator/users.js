439ebbcd99c3a4bd79ae71baf43fc9091f3fd58c
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-06-16 16:32:05 +0700

    update modal add user
    in admin page

--- Full-stack/public/javascripts/administrator/users.js
+++ Full-stack/public/javascripts/administrator/users.js
@@ -19,3 +19,32 @@ $('#js-user-type-select').change(function () {
             break;
     }
 });
+
+$('#js-right-addUser-select').change(function(){
+    if($(this).val()==="writer"){
+        $('#js-pseudonym-addUser-input').removeAttr('disabled');
+    }else{
+        $('#js-pseudonym-addUser-input').attr('disabled','true');
+    }
+});
+
+$('#js-add-user').click(function () {
+    $('#addUser__modal-background').css('display', 'block');
+})
+
+$("#btn_addUser_fail").click(function () {
+    $('#addUser__modal-background').css('display', 'none');
+});
+
+$("#btn_addUser_success").click(function (e) {
+    e.preventDefault();
+    
+    const email = $('#js-email-addUser-input').val();
+    const fullname = $('js-fullname-addUser-input').val()
+    console.log(email,fullname);
+
+    $('#addUser__modal-background').css('display', 'none');
+});
+$(document).ready(function () {
+    $("#js-birthday-input").datepicker({});
+  });
